c2598e02847099f6fde62a0196c70e054a05664b
Cloudbop <boppincolin@gmail.com>
2020-05-05 12:10:40 +0100

    added header component. inc SVG and link to home and shop page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/Homepage/Homepage';
 import ShopPage from './pages/shop/shop';
-
+import Header from './components/header/header';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />

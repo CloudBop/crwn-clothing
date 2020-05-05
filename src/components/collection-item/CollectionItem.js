b2b75a9b2e80774b24c69c49b39e498313113128
Cloudbop <boppincolin@gmail.com>
2020-05-05 11:57:32 +0100

    updated app with new shop page. inc new collection-preview&collection-item components

---
+++ src/components/collection-item/CollectionItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './collection-item.styles.scss';
+function CollectionPreview({ id, name, price, imageUrl }) {
+  return (
+    <div className={'collection-item'}>
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className="collection-footer">
+        <div className="name">{name}</div>
+        <div className="price">{price}</div>
+      </div>
+    </div>
+  );
+}
+
+export default CollectionPreview;

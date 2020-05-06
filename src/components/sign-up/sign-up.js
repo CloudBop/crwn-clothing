fcb8a1a5edcb9112cf954b0834a9b3884ebc3dcd
Cloudbop <boppincolin@gmail.com>
2020-05-06 12:51:50 +0100

    implemented sign up with email and password

--- src/components/sign-up/sign-up.js
+++ src/components/sign-up/sign-up.js
@@ -1,23 +1,103 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './header.styles.scss';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
-function header() {
-  return (
-    <div className="header">
-      <Link className="logo-container" to="/">
-        <Logo className="logo" />
-      </Link>
-      <div className="options">
-        <Link className="option" to="/shop">
-          Shop
-        </Link>
-        <Link className="option" to="/shop">
-          Contact
-        </Link>
-      </div>
-    </div>
-  );
-}
+import { auth, createUserProfileDocumentInFirestore } from '../../firebase/firebase.utils';
+import FormInput from '../form-input/form-input';
+import Button from '../../components/custom-button/custom-button';
+import './sign-up.styles.scss';
 
-export default header;
+export default class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+  }
+
+  handleSubmit = async evt => {
+    evt.preventDefault();
+    //
+    const { displayName, email, password, confirmPassword } = this.state;
+    if (password !== confirmPassword) {
+      alert("passwords don't match");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(email, password);
+      //
+      await createUserProfileDocumentInFirestore(user, { displayName });
+      // clear form
+      this.setState({
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      });
+      //
+    } catch (error) {}
+  };
+
+  handleChange = evt => {
+    const { value, name } = evt.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+    return (
+      <div className="sign-up">
+        <h2 className={'title'}>I do not have an account</h2>
+        <span>Sign up with your email and password</span>
+
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            value={displayName}
+            // id=""
+            required
+            handleChange={this.handleChange}
+            label={'Display Name'}
+          />
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            // id=""
+            required
+            handleChange={this.handleChange}
+            label={'Email'}
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            // id=""
+            required
+            onChange={this.handleChange}
+            label={'Password'}
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            // id=""
+            required
+            onChange={this.handleChange}
+            label={'Confirm Password'}
+          />
+
+          <div className="buttons">
+            <Button type="submit" value="Submit-Form">
+              Sign Up
+            </Button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
--- src/pages/sign-in-and-sign-up/SignInAndSignUp.js
+++ src/pages/sign-in-and-sign-up/SignInAndSignUp.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
+
 import './sign-in-and-sign-up.styles.scss';
 //
 function SignInAndSignUp() {
   return (
     <div>
       <SignIn />
+      <SignUp />
     </div>
   );
 }

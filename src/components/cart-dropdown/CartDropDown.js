66374e1f35f94fe2da63a2ad1025ca8d9adc5f11
Cloudbop <boppincolin@gmail.com>
2020-05-20 19:22:20 +0100

    fixed cart dropdown when no items in cart and styled

--- src/components/cart-dropdown/CartDropDown.js
+++ src/components/cart-dropdown/CartDropDown.js
@@ -9,7 +9,13 @@ import CartItem from '../cart-item/CartItem';
 function CartDropDown({ cartItems }) {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items">{cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)}</div>
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );

4ef94a0d6e3d8cf4727ad0f7a2c85cafcfd108e2
Cloudbop <boppincolin@gmail.com>
2020-05-20 16:29:11 +0100

    fixed cart.utils addItemToCart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,12 +1,12 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
+  //
   const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
-
   if (existingCartItem) {
     // return new array, remember react is immutable
     return cartItems.map(cartItem => {
-      cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem;
+      return cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem;
     });
   }
   // quantity propery is set on initialise
-  return [ ...cartItems, { cartItemToAdd, quantity: 1 } ];
+  return [ ...cartItems, { ...cartItemToAdd, quantity: 1 } ];
 };

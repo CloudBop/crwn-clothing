3a655fcbef34bb77672da72f45fe6d24b70a9a2c
Cloudbop <boppincolin@gmail.com>
2020-05-20 19:08:57 +0100

    fixed mistyped var - undefined exception

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -5,7 +5,7 @@ const selectCart = state => state.cart;
 //
 export const selectCartItems = createSelector([ selectCart ], cart => cart.cartItems);
 //
-export const selectCartHidden = createSelector([ select ], cart => cart.hidden);
+export const selectCartHidden = createSelector([ selectCart ], cart => cart.hidden);
 //
 export const selectCartItemsCountQuantity = createSelector([ selectCartItems ], cartItems =>
   cartItems.reduce((accumaltedQuantity, cartItem) => accumaltedQuantity + cartItem.quantity, 0)

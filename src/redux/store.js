d173203e30b9fc576b0e8fb32820cb57d2319969
Cloudbop <boppincolin@gmail.com>
2020-05-21 09:20:36 +0100

    installed redux-persist and implemented, see code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 // middlewares may contain multiple items
 const middlewares = [ logger ];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-export default store;
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// - leverage localStorage
+export const persistor = persistStore(store);

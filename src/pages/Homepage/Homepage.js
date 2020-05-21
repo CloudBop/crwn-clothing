1d295229858e54799dc78cd11ec06a88700ba049
Cloudbop <boppincolin@gmail.com>
2020-05-21 15:02:56 +0100

    added styled-components updated HomePage to styled components

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Directory from '../../components/directory/Directory';
-
-import './homepage.styles.scss';
+import { HomePageContainer } from './HomePage.styles';
+// import './homepage.styles.scss';
 
 function Homepage() {
   return (
-    <div className="homepage">
+    <HomePageContainer>
       <Directory />
-    </div>
+    </HomePageContainer>
   );
 }
 

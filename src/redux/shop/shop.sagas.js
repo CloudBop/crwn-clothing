b50ce4e7de227ce5c248825a176b31f9b1090fdc
Cloudbop <boppincolin@gmail.com>
2020-05-25 14:29:22 +0100

    invoked async action inside takeEvery effect in  saga generator

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,11 +1,37 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
 import ShopActionTypes from './shop.types';
 //
 //
 //
 export function* fetchCollectionsAsync() {
-  yield console.log('fetchCollectionsAsync ran as a saga');
+  try {
+    const collectionRef = firestore.collection('collections');
+    //
+    const snapshot = yield collectionRef.get();
+    // arg 1 is callback, arg 2 is args for callback
+    const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+    // put == dispatch
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+  // ABOVE IS SIMILAR TO THUNK
+  // - will only make request onMount. Not observable
+  // collectionRef
+  //   .get()
+  //   .then(snapshot => {
+  //     // promisified
+  //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+  //     // update state
+  //     dispatch(fetchCollectionsSuccess(collectionsMap));
+  //     //
+  //     // this.setState({ loading: false });
+  //   })
+  //   .catch(err => dispatch(fetchCollectionsFailure(err.message)));
 }
+
 //
 export function* fetchCollectionsStart() {
   // note - all sagas attempt to run concurrently

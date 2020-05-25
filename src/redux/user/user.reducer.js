8c0965bbef4562e50f48c415b8e877d2ee10d3ef
Cloudbop <boppincolin@gmail.com>
2020-05-25 22:18:05 +0100

    fixed signout error, wrong actiontype called in user-reducer

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -11,7 +11,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
         currentUser: action.payload,
         error: null
       };
-    case UserActionTypes.SIGN_IN_SUCCESS:
+    case UserActionTypes.SIGN_OUT_SUCCESS:
       return {
         ...state,
         currentUser: null,

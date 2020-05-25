1ae1043c15df0eeff6c0febc4417cead09c88b37
Cloudbop <boppincolin@gmail.com>
2020-05-25 11:52:45 +0100

    configured redux-saga in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,27 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
-// invoke functions in actions
-import thunk from 'redux-thunk';
-// middlewares may contain multiple items
-const middlewares = [ thunk ];
+
+// async stuff
 //
-if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
-}
+// thunks
+// import thunk from 'redux-thunk';
+// invoke functions in redux-actions
+//
+// sagas
+import createSagaMiddleware from 'redux-saga';
+const sagaMiddleware = createSagaMiddleware();
+
+// - connect middlewares
+// middlewares may contain multiple items
+const middlewares = [ sagaMiddleware ];
+//
+if (process.env.NODE_ENV === 'development') middlewares.push(logger);
+//
 //
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// redux-saga setup
+// sagaMiddleware.run()
+//
 // - leverage localStorage
 export const persistor = persistStore(store);

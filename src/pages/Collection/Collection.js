7c0c89e75a04d889683d00565ad104c934ef9bf5
Cloudbop <boppincolin@gmail.com>
2020-05-26 14:45:56 +0100

    example of observer/subscriber with useEffect

--- src/pages/Collection/Collection.js
+++ src/pages/Collection/Collection.js
@@ -3,8 +3,25 @@ import { connect } from 'react-redux';
 import { selectSingleCollection } from '../../redux/shop/shop.selector';
 import './collection.styles.scss';
 import CollectionItem from '../../components/collection-item/CollectionItem';
+// import { useEffect } from 'react';
+// import { firestore } from '../../firebase/firebase.utils';
 //
 function CollectionPage({ collection: { title, items } }) {
+  // observer / subscriber using useEffect
+  // useEffect(() => {
+  //   console.log('I AM SUBSCRIBING');
+  //   // will update on change
+  //   const unsubscribeFromCollections = firestore
+  //     .collection('collections')
+  //     .onSnapshot(snapshot => console.log(snapshot));
+
+  //   return () => {
+  //     console.log('component unmounting');
+  //     console.log('I AM UN-SUBSCRIBING');
+  //     unsubscribe(unsubscribeFromCollections);
+  //   };
+  // }, []);
+  //
   return (
     <div className="collection-page">
       <h2 className="title">{title}</h2>
